76f993f659b8f3cfe2d5be03fa2e2c87e5e7e8dc
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-07-27 03:19:17 +0300

    install.sh обновлен: скорректированы path создаваемого пользователя

--- install.sh
+++ install.sh
@@ -58,13 +58,13 @@ echo "|| данные можно не указывать - просто нажи
 echo "============================================================"
 adduser funnel_bot && echo "Пользователь funnel_bot создан"
 
+# copy files to user home path
+mv /root/easy-funnel-bot/* /home/funnel_bot/
+chown funnel_bot:funnel_bot /home/funnel_bot/
+
 # switch to non-root user & configure user environment
 runuser -l funnel_bot -c "export PATH=$HOME/.local/bin:$PATH" && echo "INSTALL.SH: HOME path установлено"
-runuser -l funnel_bot -c "cd ~ && npm i -g pm2 && npm i" && echo "INSTALL.SH: Требуемые модули библиотек npm подключены."
-
-# copy files to user home path
-mv /root/easy-funnel-bot/ /home/funnel_bot/
-chown funnel_bot:funnel_bot /home/funnel_bot/
+runuser -l funnel_bot -c "cd funnel_bot && npm i" && echo "INSTALL.SH: Требуемые модули библиотек npm подключены."
 
 # create env & set api tokens
 clear
@@ -85,38 +85,38 @@ echo "============================================================"
 read TG_TOKEN
 echo "TG_API_KEY=$TG_TOKEN" >> .env && echo "telegram токен установен" && echo "Переменные окружения записаны."
 
-mv /root/.env /home/funnel-bot/easy-funnel-bot/.env && echo "Файл окружения перенесен в корневую папку приложения."
-chown funnel_bot:funnel_bot /home/funnel-bot/easy-funnel-bot/.env && echo "Права на файл окружения переданы пользователю бота."
+mv /root/.env /home/funnel-bot/.env && echo "Файл окружения перенесен в корневую папку приложения."
+chown funnel_bot:funnel_bot /home/funnel-bot/.env && echo "Права на файл окружения переданы пользователю бота."
 
-runuser -l funnel_bot -c "pm2 start /home/funnel_bot/easy-funnel-bot/index.js" && echo "INSTALL.SH: pm2 для index.js запущен успешно."
+#runuser -l funnel_bot -c "pm2 start /home/funnel_bot/easy-funnel-bot/index.js" && echo "INSTALL.SH: pm2 для index.js запущен успешно."
 
 # install daemon systemctl service
-# cat > /etc/systemd/system/easy-funnel-bot.service << EOF
-# [Unit]
-# Description=L.A.P.S. Lab funnel Bot v0.1
-# After=syslog.target
-# After=network.target
+cat > /etc/systemd/system/easy-funnel-bot.service << EOF
+[Unit]
+Description=L.A.P.S. Lab funnel Bot v0.1
+After=syslog.target
+After=network.target
 
-# [Service]
-# Type=simple
-# User=funnel_bot
-# Group=funnel_bot
-# WorkingDirectory=/home/funnel_bot/easy-funnel-bot
-# ExecStart=node index.js
-# OOMScoreAdjust=-100
-# RestartSec=5
-# Restart=always
+[Service]
+Type=simple
+User=funnel_bot
+Group=funnel_bot
+WorkingDirectory=/home/funnel_bot/
+ExecStart=node index.js
+OOMScoreAdjust=-100
+RestartSec=5
+Restart=always
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# systemctl daemon-reload && systemctl enable easy-funnel-bot && systemctl start easy-funnel-bot && echo "Демон настроен и активирован";
+systemctl daemon-reload && systemctl enable easy-funnel-bot && systemctl start easy-funnel-bot && echo "Демон настроен и активирован";
 
 # final commands
 clear
 echo "Установка завершена."
-# echo "запрос текущего состояния бота:"
-# # systemctl status easy-funnel-bot
-# pm2 monit
+echo "запрос текущего состояния бота:"
+systemctl status easy-funnel-bot
+
 
