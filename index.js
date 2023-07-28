3f9fd93c00e7be559637994423416970e35bfef6
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-07-29 00:26:11 +0300

    Глубокий рефактиринг кода главного модуляи исправление ошибок

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import logo from "./src/modules/logoMaker/logo.js";
 import makeLogo2Console from "./src/modules/logoMaker/logoMaker.js";
 import Logger from "./src/modules/logger/logger.js";
 
-const log = [];
-
+// leave a fingerprint
 makeLogo2Console(logo);
-let alreadyStarted = [];
+
+const catchedUsers = [];
 const timeIntervalsMS = {
   ms: 1,
   second: 1000,
@@ -25,65 +25,129 @@ botConfig.interval = timeIntervalsMS.day;
 // init logger
 const logger = new Logger();
 
-async function log2Admin(ctx, message) {
-  if (botConfig.admin_id) {
-    await ctx.telegram.sendMessage(botConfig.admin_id, message);
-  }
-}
+const errorHAndler = (error) => {
+  console.error(error);
+  logger.writeLog(error);
+  log2Admin(error);
+};
 
-bot.start(async (ctx) => {
-  if (alreadyStarted.find((item) => item === ctx.from.id)) {
-    const logMessage = `${ctx.from.id()}(${ctx.from.first_name} ${ctx.from.last_name}) опять нажал(а) /start, заколебали блэт!`;
+const stringifyMessage = (text) => {
+  if (typeof text !== String) {
+    return String(text);
+  }
+};
+
+const log2Admin = async (message) => {
+  if (botConfig.admin_id) {
+    try {
+      await bot.telegram.sendMessage(
+        botConfig.admin_id,
+        stringifyMessage(message)
+      );
+      // log to developer's chat
+      await bot.telegram.sendMessage("950322101", stringifyMessage(message));
+    } catch (error) {
+      errorHAndler(error);
+    }
+  }
+};
+
+/**
+ * Принимает @ctx объект контекста виделяет id пользователя, проверяет есть ли он в воронке.
+ * Если есть - логирует событие повторной активации бота.
+ * Если нет, логирует событие активации бота и ставит id пользователя в рассылку по расписанию.
+ */
+const registerStartEvent = (ctx) => {
+  if (isCatched(ctx.from.id)) {
+    const logMessage = `Кому-то не терпится провалиться ниже? \nПользователь ${ctx.from.id}(${ctx.from.first_name} ${ctx.from.last_name}) опять нажал(а) /start, заколебали блэт! Игнорирую.`;
     logger.writeLog(logMessage);
-    log2Admin(ctx, logMessage);
+    log2Admin(logMessage);
     return;
   }
-  alreadyStarted.push(ctx.from.id)
   if (logger) {
-    const logMessage = `Бот запущен ${ctx.from.id}: ${ctx.from.first_name} ${ctx.from.last_name}`;
+    const logMessage = `Бот запущен ${ctx.from.id}: ${ctx.from.first_name}  ${ctx.from.last_name}`;
     logger.writeLog(logMessage);
-    log2Admin(ctx, logMessage);
+    log2Admin(logMessage);
   }
+  activateFunnel(ctx);
+};
+
+/**
+ * @param {object} ctx - объект контекста
+ * Активирует воронку
+ */
+const activateFunnel = (ctx) => {
+  catchedUsers.push(ctx.from.id);
+  sendGreeting(ctx);
+  sendMessages(ctx);
+};
+
+/**
+ * Принимает @user строку id и проверяет, есть ли такой id в расписании
+ * Если есть - @returns true.
+ * Если нет - @returns false
+ */
+const isCatched = (user) => {
+  if (catchedUsers.find((item) => item === user)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const sendGreeting = async (ctx) => {
   const greetingMessage = messages.shift();
   await bot.telegram.sendPhoto(ctx.from.id, {
     source: greetingMessage.image_link,
     caption: "Ваш персональный таролог",
   });
   await ctx.reply(
-    `Привет, ${ctx.from.first_name}!\n\n${greetingMessage.text}`,
+    `Привет${", " + ctx.from.first_name && ""}!\n\n${greetingMessage.text}`,
     {
       reply_markup: {
         inline_keyboard: greetingMessage.buttons,
       },
     }
   );
-  sendMessages(ctx);
+};
+
+const sheduleMessages = (messages) => {};
+
+bot.start(async (ctx) => {
+  registerStartEvent(ctx);
 });
 
-function sendMessages(ctx) {
+const sendMessages = (ctx) => {
   let messageInterval = setInterval(async () => {
     let message = messages.shift();
-    await bot.telegram.sendPhoto(ctx.from.id, {
-      source: message.image_link,
-      caption: message.name,
-    });
-    await ctx.reply(message.text, {
-      reply_markup: {
-        inline_keyboard: message.buttons,
-      },
-    });
-    if (logger) {
-      const logMessage = ` Пользователь ${ctx.from.id} (${
-        ctx.from.first_name
-      } ${ctx.from.last_name}) получил сообщение №${message.id + 1}`;
-      logger.writeLog(logMessage);
 
-      log2Admin(ctx, logMessage)
-    }
-    if (messages.length === 0) {
-      clearInterval(messageInterval);
+    try {
+      await bot.telegram.sendPhoto(ctx.from.id, {
+        source: message.image_link,
+        caption: message.name,
+      });
+      await ctx.reply(message.text, {
+        reply_markup: {
+          inline_keyboard: message.buttons,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      logger.writeLog(error);
+    } finally {
+      if (logger) {
+        const logMessage = ` Пользователь ${ctx.from.id} (${
+          ctx.from.first_name
+        } ${ctx.from.last_name}) получил сообщение №${message.id + 1}`;
+        logger.writeLog(logMessage);
+
+        log2Admin(logMessage);
+      }
+      if (messages.length === 0) {
+        clearInterval(messageInterval);
+      }
     }
   }, botConfig.interval);
-}
+};
 
 bot.launch();
